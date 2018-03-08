ea4aa90368f63865615b1a267c2e8c7a80deef1b
G0LD <37147752+goldenboyyyyy@users.noreply.github.com>
2018-03-08 12:02:59 +0100

    -> constraint layout

--- app/src/main/java/com/cafe/cafe_pre_alpha_1/MainActivity.java
+++ app/src/main/java/com/cafe/cafe_pre_alpha_1/MainActivity.java
@@ -1,13 +1,31 @@
 package com.cafe.cafe_pre_alpha_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mRegisterLink ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRegisterLink = (TextView) findViewById(R.id.link_register);
+
+        mRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent RegisterActivityIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(RegisterActivityIntent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cafe/cafe_pre_alpha_1/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.cafe.cafe_pre_alpha_1;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+
+}
